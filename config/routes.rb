a62d25c0b3e66913127b77a1e3e35032891741bf
Carole Piette <carole@conscients.com>
2016-11-15 16:03:58 +0100

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # read all tasks
+  # get "tasks" => "tasks#index"
+  # # read one task
+  # get "tasks/:id" => "tasks#show"
+  # # create a task : get the form to create the task
+  # get "tasks/new" => "tasks#new"
+  # # create a task : post a new task
+  # post "tasks" => "tasks#create"
+  # # update a task : get the form to edit an existing task
+  # get "tasks/:id/edit" => "tasks#edit"
+  # #update a task : update an existing task
+  # patch "tasks/:id" => "tasks#update"
+  # # delete an existing task
+  # delete "tasks/:id" => "tasks#destroy"
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+#questions tasks au pluriel car dans db?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: "Course", description: "Legume", priority: 3)
+Task.create(title: "Diner", description: "fruits", priority: 2)
